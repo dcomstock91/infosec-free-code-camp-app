3da790ea38ead0ae2a446812915745200ea83fda
DaveC <davec@kali.Kali>
2024-03-12 17:38:35 -0400

    hsts

--- myApp.js
+++ myApp.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
+const timeInSeconds = 7776000;
 
 app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action: 'deny'}));
+app.use(
+  helmet.frameguard({
+    action: 'deny'
+  }));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
+app.use(
+  helmet.hsts({
+    maxAge: timeInSeconds, force: true
+  })
+)
 
 
 

177a4d7ab175268ced6f75668052547400277ddf
DaveC <davec@kali.Kali>
2024-03-12 16:43:22 -0400

    noSniff

--- myApp.js
+++ myApp.js
@@ -3,10 +3,9 @@ const helmet = require('helmet');
 const app = express();
 
 app.use(helmet.hidePoweredBy());
-
 app.use(helmet.frameguard({action: 'deny'}));
-
 app.use(helmet.xssFilter())
+app.use(helmet.noSniff())
 
 
 
